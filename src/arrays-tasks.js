90807837c0453fe93494ce5c04582e2f1041bf0f
Absolute-2004 <mixalobach@gmail.com>
2025-01-13 21:39:00 +0300

    feat: pass task with the function removeFalsyValues

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -90,8 +90,16 @@ function findAllOccurrences(arr, item) {
  *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
  *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
  */
-function removeFalsyValues(/* arr */) {
-  throw new Error('Not implemented');
+function removeFalsyValues(arr) {
+  return arr.filter(
+    (element) =>
+      element !== false &&
+      element !== null &&
+      element !== 0 &&
+      element !== '' &&
+      element !== undefined &&
+      !Number.isNaN(element)
+  );
 }
 
 /**
